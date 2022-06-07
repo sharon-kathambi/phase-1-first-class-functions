52697627f994dd2013b487b1167cd28149fae79d
Sharon Mwenda <smkathambi@gmail.com>
2022-06-07 20:22:11 +0300

    commit tree 0d29f490b9a1c96326ca8feffbf2de56a740dd9e

---
+++ index.js
@@ -0,0 +1,18 @@
+function sum(num1, num2){
+    return num1+num2;
+}
+function receivesAFunction(sum){
+    return sum();
+}
+ const divide = function(){
+     num3/num4;
+ }
+function returnsANamedFunction (){
+    return divide;
+}
+
+function returnsAnAnonymousFunction(){
+    return function() {
+        return 2000/100;
+    }
+}
